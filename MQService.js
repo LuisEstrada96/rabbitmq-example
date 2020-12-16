1ffabf902b6b0c96c160679503d4401699d513f9
Luis Estrada <inmediatum@luisestrada.local>
2020-12-16 11:36:11 -0600

    initial commit

---
+++ MQService.js
@@ -0,0 +1,45 @@
+const amqp = require("amqplib/callback_api");
+const CONN = "amqp://localhost";
+
+let CH;
+
+amqp.connect(CONN, (err, conn) => {
+    conn.createChannel((error, channel) => {
+        CH = channel;
+        consumer();
+    })
+})
+
+function publishToQueue (queue, msg) {
+    CH.sendToQueue(queue, Buffer.from(msg), { persistent: true } );
+}
+
+function consumer(){
+    CH.consume('messages', ( msg ) => {
+        handleConsume(msg)
+        .then(()=>{
+            CH.ack(msg);
+        })
+        .catch( err => CH.nack(msg))
+    })
+}
+
+function handleConsume(payload){
+    return new Promise(function (resolve, reject) { 
+        setTimeout(() => {
+            console.log('-------');
+            console.log(`Msg: ${ payload.content }`)
+            return resolve();
+        }, 2000)
+    });
+}
+
+var stdin = process.openStdin();
+
+stdin.addListener("data", (data) => {
+    publishToQueue('messages', data);
+});
+
+
+
+
